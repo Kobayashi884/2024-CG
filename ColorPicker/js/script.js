cb35a6bcbfd7042fb72059f9908e9d6b4e89a23c
Kobayashi884 <153569768+Kobayashi884@users.noreply.github.com>
2024-05-27 16:21:00 +0900

    Add files via upload

---
+++ ColorPicker/js/script.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () =>{
+
+    document.body.style.backgroundColor = color.value;
+
+//    text.textContent = `カラーコード：${color.value}`;
+
+    if(color.value === '#ffffff'){
+        text.textContent = `カラーコード: ${color.value} (white)`;
+    }else{
+        text.textContent = `カラーコード：${color.value}`;
+    }
+}
+
+color.addEventListener('input', colorBg);
